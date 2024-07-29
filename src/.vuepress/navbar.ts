0b75f10c764dcc657c44d3f06107735cf7f155f7
JERRY-SYSTEM <jerryaidiannao@126.com>
2024-07-29 10:30:26 +0800

    6

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,22 +1,26 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-    "/",
-    {text: "文档", icon: "read", link: "/docs/"},
-    {text: "下载", icon: "link", link: "/download"},
-    {text: "捐赠", icon: "selection", link: "/donate/"},
-    {text: "友链", icon: "community", link: "/friends/"},
-    {
-        text: "关于", icon: "people", children: [{
-            text: "Glow Studio",
-            link: "https://studio.000708.xyz"
-        }, {
-            text: "微信公众号",
-            link: "https://yanxuan.nosdn.127.net/41c14c219f06fcc1ceefc861d3c391a1.jpg"
-        }, {
-            text: "QQ交流群",
-            link: "https://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=EEh7V2N3pRqAWGrPWW2kSTwdKmZHm2SD&authKey=wwaCNoL3F%2Bi5wbyUy7LL88G1L%2FC29ARggZ6PJa2ue5%2BAZMyLmlDsI6a9bP6%2BQfOK&noverify=0&group_code=948504002"
-        }]
-    }
+  { text: "主页", icon: "house", link: "/" },
+  { text: "程序人生", icon: "coffee", link: "/program/" },
+  { text: "算法笔记", icon: "book", link: "/algorithm/" },
+  { text: "开源项目", icon: "github", link: "/open-source-project/" },
+  {
+    text: "技术小结",
+    icon: "books",
+    children: [
+      { text: "计算机", icon: "computer", link: "/computer/" },
+      { text: "常用框架", icon: "framework", link: "/framework/" },
+      { text: "架构设计", icon: "server", link: "/architecture/" },
+    ],
+  },
+  {
+    text: "关于本站",
+    icon: "about",
+    children: [
+      { text: "关于作者", icon: "about", link: "/intro.html"},
+      { text: "更新历史", icon: "history", link: "/timeline/"},
+    ],
+  },
 ]);
 
